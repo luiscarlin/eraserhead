db49ef96e1735391cebd8bfd1f85919cb7a7eaf1
Luis Carlin <luiscarlin@gmail.com>
2019-10-19 08:48:11 -0400

    use state to keep track if its opened or not

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './mobile-menu.scss'
 
 function MobileMenu() {
@@ -6,9 +6,10 @@ function MobileMenu() {
 }
 
 function Burger() {
+  let open = useState(false)
   return (
     <button className="burger">
-      <div className="bar top open" />
+      <div className="bar top" />
       <div className="bar middle" />
       <div className="bar bottom" />
     </button>
