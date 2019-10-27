5a7257698729d29b454ac65a0b253aec1ca4ce72
Luis Carlin <luiscarlin@gmail.com>
2019-10-26 22:15:10 -0400

    use react component to lazy load image

--- src/components/sections/Hero/Hero.js
+++ src/components/sections/Hero/Hero.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './Header'
+import LazyHero from 'react-lazy-hero'
+import background from '../../../assets/target-person.jpg'
 import './hero.scss'
 
 function Hero() {
   return (
     <section className="hero">
-      <Header />
-      <div class="hero-title">EraserHead</div>
+      <div className="header-container">
+        <Header />
+      </div>
+      <LazyHero imageSrc={background} color="black" minHeight={'100%'}>
+        <h1 className="title">EraserHead</h1>
+      </LazyHero>
     </section>
   )
 }
